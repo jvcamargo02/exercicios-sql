cdfb1b587477a470da807c4b6c0bd5c44641f17c
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-07-26 19:06:12 -0300

    Init

---
+++ pratica-join.sql
@@ -0,0 +1,19 @@
+ /*### Questão 1:
+
+Utilizando uma *query*, obtenha todos os usuários (`users`) que vivem na cidade (`cities`) cujo nome seja “**Rio de Janeiro**”.
+
+A resposta deve vir no formato:
+
+```
+|---id---|---name---|-------city-------|
+    1        João      Rio de Janeiro
+    2        Maria     Rio de Janeiro
+   ...        ...            ...
+```
+
+**OBS:** a coluna `id` se refere ao id do usuário
+
+**Dica:** utilize apelidos para os nomes de colunas ficarem como você deseja (ex: `SELECT cities.name AS city`)
+
+**Dica:** na query, todo nome de coluna que tenha letras maiúsculas precisa estar entre aspas duplas (ex: `ON users."cityId" = ...`). Repare que somente o nome da coluna fica entre aspas duplas.  */
+
